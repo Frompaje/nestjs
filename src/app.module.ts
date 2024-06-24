9af13f6e44a00649177d8be045195eade9a0aa09
frompaje <yanedwards11@gmail.com>
2024-06-24 17:15:19 -0300

    confi model env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.services';
 import { CreateAccountController } from './controllers/create-account.controller';
+import { ConfigModule } from '@nestjs/config';
+import { object } from 'zod';
+import { envSchema } from './en';
+
+
+
 
 @Module({
+  imports: [ConfigModule.forRoot({
+    validate: env => envSchema.parse(env),
+    isGlobal: true
+  })],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })

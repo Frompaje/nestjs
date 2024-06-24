7545239e63a7629d6dbbf42bf12dfd521192780e
frompaje <yanedwards11@gmail.com>
2024-06-24 19:10:00 -0300

    create controller question

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,6 +1,20 @@
-import { Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { CurrentUser } from "src/auth/current-user-decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { UserPayload } from "src/auth/jwt.strategy";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipes";
 import { PrismaService } from "src/prisma/prisma.services";
+import { z } from "zod";
+
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string()
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
 
 
 
@@ -10,7 +24,21 @@ export class CreateQuestionController {
   constructor(private readonly prisma: PrismaService) { }
 
   @Post()
-  async handle() {
-    return "ok"
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload) {
+    const { title, content } = body
+    const userId = user.sub
+
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug: "asd"
+      }
+    })
+
   }
 }

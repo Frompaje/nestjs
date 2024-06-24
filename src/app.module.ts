3582e44907bde4a30a6671e78288ffc3824cd4e6
frompaje <yanedwards11@gmail.com>
2024-06-24 18:50:52 -0300

    protecting routes with guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { envSchema } from './env';
 import { PrismaService } from './prisma/prisma.services';
 import { AuthModule } from './auth/auth.module';
 import { AuthenticateController } from './controllers/authenticate.controller';
+import { CreateQuestionController } from './controllers/create-question.controller';
 
 
 
@@ -15,7 +16,7 @@ import { AuthenticateController } from './controllers/authenticate.controller';
     isGlobal: true
   }),
     AuthModule],
-  controllers: [CreateAccountController, AuthenticateController],
+  controllers: [CreateAccountController, AuthenticateController, CreateQuestionController],
   providers: [PrismaService],
 })
 export class AppModule { }
